564dd8365cba3e7e718ad156b85add24359a7d61
Lewis Luther-Braun <lewis.braun@googlemail.com>
2018-07-12 10:33:27 +0100

    Added describe block for each test class

--- test/booking.test.js
+++ test/booking.test.js
@@ -30,54 +30,56 @@ provided)
 */
 
 var booking = new Booking(new Date("2018-09-01"), new Date("2018-09-05"))
+describe('Booking Class', function() {
+    describe('Booking creation', function() {
+        it("should create a new booking with the correct start and end date", function(){
+            expect(booking.startDate).to.eql(new Date("2018-09-01"));
+            expect(booking.endDate).to.eql(new Date("2018-09-05"));
+        });
+    })
 
-describe('Booking creation', function() {
-    it("should create a new booking with the correct start and end date", function(){
-        expect(booking.startDate).to.eql(new Date("2018-09-01"));
-        expect(booking.endDate).to.eql(new Date("2018-09-05"));
-    });
-})
+    describe('Day calculation', function() {
+        it("should correctly calculate the length of time taken", function(){
+            expect(booking.numberOfDays()).to.eql(5);
+        });
+    })
 
-describe('Day calculation', function() {
-    it("should correctly calculate the length of time taken", function(){
-        expect(booking.numberOfDays()).to.eql(5);
-    });
-})
+    describe('Initial Authorisation Check', function() {
+        it("should not be authorised when created", function(){
+            expect(booking.isAuthorised()).to.eql(false);
+        });
+        it("should have no linked authoriser", function(){
+            expect(booking.authorisedBy()).to.eql(null);
+        });
+        it("should not have an authorised date", function(){
+            expect(booking.authorisedOn()).to.eql(null);
+        });
+    })
 
-describe('Initial Authorisation Check', function() {
-    it("should not be authorised when created", function(){
-        expect(booking.isAuthorised()).to.eql(false);
-    });
-    it("should have no linked authoriser", function(){
-        expect(booking.authorisedBy()).to.eql(null);
-    });
-    it("should not have an authorised date", function(){
-        expect(booking.authorisedOn()).to.eql(null);
-    });
-})
+    describe('Authorisation Creation', function() {
+        it("should not be authorised when created", function(){
+            booking.authorise("Mr Boss Man")
+            expect(booking.isAuthorised()).to.eql(true);
+        });
+        it("should have no linked authoriser", function(){
+            expect(booking.authorisedBy()).to.eql("Mr Boss Man");
+        });
+        it("should have a date equal to today", function(){
+            expect(booking.authorisedOn()).to.eql(new Date().toLocaleDateString());
+        });
+    })
 
-describe('Authorisation Creation', function() {
-    it("should not be authorised when created", function(){
-        booking.authorise("Mr Boss Man")
-        expect(booking.isAuthorised()).to.eql(true);
-    });
-    it("should have no linked authoriser", function(){
-        expect(booking.authorisedBy()).to.eql("Mr Boss Man");
-    });
-    it("should have a date equal to today", function(){
-        expect(booking.authorisedOn()).to.eql(new Date().toLocaleDateString());
-    });
-})
+    describe('Further Authorisation Creation', function() {
+        it("should not be authorised when created", function(){
+            booking.authorise("Mr Boss Man Again", new Date("2018-07-01"))
+            expect(booking.isAuthorised()).to.eql(true);
+        });
+        it("should have no linked authoriser", function(){
+            expect(booking.authorisedBy()).to.eql("Mr Boss Man Again");
+        });
+        it("should be able to give a second date arguement", function(){
+            expect(booking.authorisedOn()).to.eql(new Date("2018-07-01"));
+        });
+    })
 
-describe('Further Authorisation Creation', function() {
-    it("should not be authorised when created", function(){
-        booking.authorise("Mr Boss Man Again", new Date("2018-07-01"))
-        expect(booking.isAuthorised()).to.eql(true);
-    });
-    it("should have no linked authoriser", function(){
-        expect(booking.authorisedBy()).to.eql("Mr Boss Man Again");
-    });
-    it("should be able to give a second date arguement", function(){
-        expect(booking.authorisedOn()).to.eql(new Date("2018-07-01"));
-    });
 })
--- test/employee.test.js
+++ test/employee.test.js
@@ -15,44 +15,68 @@ employee.holidayAllowance ​//=> 25
 
 var employee = new Employee("E123", "joe bloggs", "joe@bloggs.com", 25)
 
-describe('Employee creation', function() {
-    it("should create an employee correctly", function(){
-        expect(employee.payrollNo).to.eql("E123");
-        expect(employee.name).to.eql("Joe Bloggs");
-        expect(employee.email).to.eql("joe@bloggs.com");
-        expect(employee.bookings).to.eql([]);
-        expect(employee.holidayAllowance).to.eql(25);
-    });
+describe('Employee Class', function() {
+    describe('Employee creation', function() {
+        it("should create an employee correctly", function(){
+            expect(employee.payrollNo).to.eql("E123");
+            expect(employee.name).to.eql("Joe Bloggs");
+            expect(employee.email).to.eql("joe@bloggs.com");
+            expect(employee.bookings).to.eql([]);
+            expect(employee.holidayAllowance).to.eql(25);
+        });
+
+    })
+
+    /*
+    employee.bookings = ​'nonsense'​ ​//=> Raise Exception to prevent
+    overwriting (no need to write a test for this)
+    */
+
+    describe('Employee days taken initial setup', function() {
+        it("should create days remaining", function(){
+            expect(employee.daysRemaining()).to.eql(25);
+            expect(employee.daysBooked()).to.eql(0);
+            expect(employee.daysBookedAndAuthorised()).to.eql(0);
+        });
+    })
+
+    /*
+    employee.daysRemaining() ​//=> 25
+    employee.daysBooked() ​//=> 0
+    employee.daysBookedAndAuthorized() ​//=> 0
+
+    */
+
+    employee.makeBooking("2018-09-01","2018-09-05")
+    employee.makeBooking("2018-01-01","2018-01-05")
+
+    describe('Employee Bookings', function() {
+        it("create a booking and update the various", function(){
+            expect(employee.daysRemaining()).to.eql(15);
+            expect(employee.daysBooked()).to.eql(5);
+            expect(employee.daysBookedAndAuthorised()).to.eql(0);
+        });
+    })
+
+    /*
+    employee.makeBooking(​"2018-09-01"​, ​"2018-09-05"​) ​//=> a Booking object
+    with these start/end dates.
+    employee.makeBooking(​"2018-01-01"​, ​"2018-01-05"​) ​//=> a Booking object
+    with these start/end dates.
+    employee.daysRemaining() ​//=> 15
+    employee.daysBooked() ​//=> 5
+    employee.daysBookedAndAuthorized() ​//=> 0
+    */
+
+    /*
+    employee.futureBookings() ​//=> [Booking] Array of all future bookings
+    employee.pastBookings() ​//=> [Booking] Array of all past bookings
+    employee.futureBookings()[​0​].authorize(​"Mr Boss Man"​) ​// Authorise a
+    booking as before.
+    employee.daysBooked() ​//=> 5
+    employee.daysBookedAndAuthorized() ​//=> 5
+    employee.futureBookings(​true​) ​//=> Only include authorized bookings
+    employee.pastBookings(​true​) ​//=> Only include authorized bookings
+    */
 
 })
-
-
-/*
-employee.bookings = ​'nonsense'​ ​//=> Raise Exception to prevent
-overwriting (no need to write a test for this)
-*/
-
-
-
-
-/*
-employee.daysRemaining() ​//=> 25
-employee.daysBooked() ​//=> 0
-employee.daysBookedAndAuthorized() ​//=> 0
-
-employee.makeBooking(​"2018-09-01"​, ​"2018-09-05"​) ​//=> a Booking object
-with these start/end dates.
-employee.makeBooking(​"2018-01-01"​, ​"2018-01-05"​) ​//=> a Booking object
-with these start/end dates.
-employee.daysRemaining() ​//=> 15
-employee.daysBooked() ​//=> 5
-employee.daysBookedAndAuthorized() ​//=> 0
-employee.futureBookings() ​//=> [Booking] Array of all future bookings
-employee.pastBookings() ​//=> [Booking] Array of all past bookings
-employee.futureBookings()[​0​].authorize(​"Mr Boss Man"​) ​// Authorise a
-booking as before.
-employee.daysBooked() ​//=> 5
-employee.daysBookedAndAuthorized() ​//=> 5
-employee.futureBookings(​true​) ​//=> Only include authorized bookings
-employee.pastBookings(​true​) ​//=> Only include authorized bookings
-*/
