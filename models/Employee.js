4a371fb2cf04ec722f5b303a85ffc90131187cf6
Lewis Luther-Braun <lewis.braun@googlemail.com>
2018-07-12 13:29:02 +0100

    Completed Employee class

--- models/Employee.js
+++ models/Employee.js
@@ -1,3 +1,4 @@
+const Booking = require("./Booking");
 class Employee{
     constructor(payrollNumber,name,emailAddress,allowance){
         this.payrollNo = payrollNumber
@@ -5,6 +6,9 @@ class Employee{
         this.email = emailAddress
         this._bookings =[]
         this.holidayAllowance = allowance
+        this.daysBookedOff = 0
+        this.daysAuthorised = 0
+
     }
 
     set bookings (booking) {
@@ -16,8 +20,47 @@ class Employee{
         return this._bookings
     }
 
+    daysRemaining(){
+        return this.holidayAllowance
+    }
+
+    daysBooked(){
+        return this.daysBookedOff
+    }
+
+    daysBookedAndAuthorised(){
+        this.daysAuthorised = 0
+        for(var booking of this._bookings){
+            if(booking.isAuthorised()){
+                this.daysAuthorised += booking.numberOfDays()
+            }
+        }
+        return this.daysAuthorised
+    }
+
     _capitalize_String(str){
     return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    }
+
+    makeBooking(startDate, endDate){
+        var booking = new Booking(startDate,endDate)
+        this.holidayAllowance -= booking.numberOfDays()
+        this.daysBookedOff += booking.numberOfDays()
+        this._bookings.push(booking)
+    }
+
+    futureBookings(returnOnlyAuthorised){
+        switch(returnOnlyAuthorised){
+            case true: return this._bookings.filter(booking => (booking.startDate > new Date()) && (booking.isAuthorised() == true));
+            default: return this._bookings.filter(booking => booking.startDate > new Date());
+        }
+    }
+
+    pastBookings(returnOnlyAuthorised){
+        switch(returnOnlyAuthorised){
+            case true: return this._bookings.filter(booking => ((booking.startDate < new Date()) && (booking.isAuthorised() == true)));
+            default: return this._bookings.filter(booking => booking.startDate < new Date());
+        }
     }
 }
 
